e0dc2c150515e6fc27f5406a66d99c1bbe1fd6fd
Antidale <antidale@tellah.life>
2025-06-10 15:12:58 -0700

    update to use a centralized way to call the api

---
+++ racingway/fe_info.py
@@ -0,0 +1,28 @@
+'''
+Centralized code for calling endpoints for the fe info api
+'''
+import requests
+import os
+
+base_url = "https://free-enterprise-info-api.herokuapp.com/api/"
+header = { 'Api-Key': os.environ['FE_Info_Api_Key'] }
+
+is_debug = os.environ['racingway_debug'] is not None
+
+if (is_debug):
+    base_url = "https://localhost:5001/api/Races"
+    header = { 'Api-Key': os.environ['FE_Info_Local_Key'] }
+    
+
+class FeInfoApi():
+    async def log_rolled_seed(payload):
+        url = base_url + "Seed"
+        return requests.post(url=url, json=payload, headers=header)
+    
+    async def log_race_room(payload):
+        url = base_url + "Seed"
+
+        if(is_debug):
+            return requests.post(url=url, json=payload, headers=header, verify=False)
+        else:
+            return requests.post(url=url, json=payload, headers=header)
--- racingway/log_race.py
+++ racingway/log_race.py
@@ -1,8 +1,7 @@
 '''
 Code for logging races
 '''
-import requests
-import os
+from .fe_info import FeInfoApi
 
 class LogRaceRoom():
     def new_seed_log(room_name, creator_id, description, goal):
@@ -24,7 +23,4 @@ class RaceLogger():
         if creator is not None:
             creator_id = creator.get('full_name')
         payload = LogRaceRoom.new_seed_log(room_name, creator_id, description, goal)
-        # url = "https://localhost:5001/api/Races"
-        url = "https://free-enterprise-info-api.herokuapp.com/api/Races"
-        header = { 'Api-Key': os.environ['FE_Info_Api_Key'] }
-        return requests.post(url=url, json=payload, headers=header)
+        return await FeInfoApi.log_race_room(payload)
